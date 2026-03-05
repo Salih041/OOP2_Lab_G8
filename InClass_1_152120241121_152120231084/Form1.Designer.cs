9e06bd163e2f660d081d9e30e55bc09a208daf86
Salih041 <salihozbk41@gmail.com>
2026-03-05 12:18:48 +0300

    ilk lab

---
+++ InClass_1_152120241121_152120231084/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.lbl = new System.Windows.Forms.Label();
+            this.btnShowName = new System.Windows.Forms.Button();
+            this.btnSwap = new System.Windows.Forms.Button();
+            this.btnColor = new System.Windows.Forms.Button();
+            this.btnSize = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(287, 168);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(283, 20);
+            this.txtName.TabIndex = 0;
+            this.txtName.Text = "Name";
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(287, 215);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(283, 20);
+            this.txtSurname.TabIndex = 1;
+            this.txtSurname.Text = "Surname";
+            // 
+            // lbl
+            // 
+            this.lbl.BackColor = System.Drawing.Color.White;
+            this.lbl.Location = new System.Drawing.Point(287, 258);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(283, 44);
+            this.lbl.TabIndex = 2;
+            this.lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl.Click += new System.EventHandler(this.lbl_Click);
+            // 
+            // btnShowName
+            // 
+            this.btnShowName.BackColor = System.Drawing.Color.Gold;
+            this.btnShowName.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnShowName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnShowName.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.btnShowName.Location = new System.Drawing.Point(287, 316);
+            this.btnShowName.Name = "btnShowName";
+            this.btnShowName.Size = new System.Drawing.Size(108, 35);
+            this.btnShowName.TabIndex = 3;
+            this.btnShowName.Text = "Display Name";
+            this.btnShowName.UseVisualStyleBackColor = false;
+            this.btnShowName.Click += new System.EventHandler(this.btnShowName_Click);
+            // 
+            // btnSwap
+            // 
+            this.btnSwap.BackColor = System.Drawing.Color.Gold;
+            this.btnSwap.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSwap.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSwap.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.btnSwap.Location = new System.Drawing.Point(462, 316);
+            this.btnSwap.Name = "btnSwap";
+            this.btnSwap.Size = new System.Drawing.Size(108, 35);
+            this.btnSwap.TabIndex = 4;
+            this.btnSwap.Text = "Swap Names";
+            this.btnSwap.UseVisualStyleBackColor = false;
+            this.btnSwap.Click += new System.EventHandler(this.btnSwap_Click);
+            // 
+            // btnColor
+            // 
+            this.btnColor.BackColor = System.Drawing.Color.Gold;
+            this.btnColor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnColor.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnColor.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.btnColor.Location = new System.Drawing.Point(287, 369);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(108, 35);
+            this.btnColor.TabIndex = 5;
+            this.btnColor.Text = "Change Color";
+            this.btnColor.UseVisualStyleBackColor = false;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // btnSize
+            // 
+            this.btnSize.BackColor = System.Drawing.Color.Gold;
+            this.btnSize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSize.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSize.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.btnSize.Location = new System.Drawing.Point(462, 369);
+            this.btnSize.Name = "btnSize";
+            this.btnSize.Size = new System.Drawing.Size(108, 35);
+            this.btnSize.TabIndex = 6;
+            this.btnSize.Text = "Change Size";
+            this.btnSize.UseVisualStyleBackColor = false;
+            this.btnSize.Click += new System.EventHandler(this.btnSize_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(144, 157);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(137, 87);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(380, 75);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(94, 87);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(860, 536);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnSize);
+            this.Controls.Add(this.btnColor);
+            this.Controls.Add(this.btnSwap);
+            this.Controls.Add(this.btnShowName);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.txtName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.Button btnShowName;
+        private System.Windows.Forms.Button btnSwap;
+        private System.Windows.Forms.Button btnColor;
+        private System.Windows.Forms.Button btnSize;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ InClass_1_152120241121_152120231084/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        private int counter = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            TasarimAyarlari();
+        }
+
+        private void TasarimAyarlari()
+        {
+            btnShowName.Cursor = Cursors.Hand;
+            btnSwap.Cursor = Cursors.Hand;
+            btnColor.Cursor = Cursors.Hand;
+            btnSize.Cursor = Cursors.Hand;
+
+            txtName.Enter += TxtName_Enter;
+            txtName.Leave += TxtName_Leave;
+
+            txtSurname.Enter += TxtSurname_Enter;
+            txtSurname.Leave += TxtSurname_Leave;
+
+            lbl.Text = "";
+        }
+
+        private void TxtName_Enter(object sender, EventArgs e)
+        {
+            if (txtName.Text == "Name")
+            {
+                txtName.Text = "";
+                txtName.ForeColor = Color.Black;
+            }
+        }
+
+        private void TxtName_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtName.Text))
+            {
+                txtName.Text = "Name";
+                txtName.ForeColor = Color.Gray;
+            }
+        }
+
+        private void TxtSurname_Enter(object sender, EventArgs e)
+        {
+            if (txtSurname.Text == "Surname")
+            {
+                txtSurname.Text = "";
+                txtSurname.ForeColor = Color.Black;
+            }
+        }
+
+        private void TxtSurname_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtSurname.Text))
+            {
+                txtSurname.Text = "Surname";
+                txtSurname.ForeColor = Color.Gray;
+            }
+        }
+
+        private void btnShowName_Click(object sender, EventArgs e)
+        {
+            lbl.Text = txtName.Text + " " + txtSurname.Text;
+        }
+
+        private void btnSwap_Click(object sender, EventArgs e)
+        {
+            string temp = txtName.Text;
+
+            txtName.Text = txtSurname.Text;
+
+            txtSurname.Text = temp;
+
+            lbl.Text = txtName.Text + " " + txtSurname.Text;
+        }
+
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            switch (counter)
+            {
+                case 0:
+                    lbl.ForeColor = Color.Blue;
+                    break;
+                case 1:
+                    lbl.ForeColor = Color.Black;
+                    break;
+                case 2:
+                    lbl.ForeColor = Color.Pink;
+                    break;
+                case 3:
+                    lbl.ForeColor = Color.Orange;
+                    break;
+            }
+
+            counter = (counter + 1) % 4;
+        }
+
+        private void btnSize_Click(object sender, EventArgs e)
+        {
+            if (lbl.Font.Size == 15)
+            {
+                lbl.Font = new Font(lbl.Font.FontFamily, 9, lbl.Font.Style);
+            }
+            else
+            {
+                lbl.Font = new Font(lbl.Font.FontFamily, 15, lbl.Font.Style);
+            }
+        }
+
+        private void lbl_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
